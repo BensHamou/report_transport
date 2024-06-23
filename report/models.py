99b2e59a56c4566506b48b2167a60586da8d0819
mohammed.benslimane <benshamou@gmail.com>
2024-06-23 16:11:16 +0100

    allow multiple users

--- report/models.py
+++ report/models.py
@@ -28,7 +28,7 @@ class Tonnage(models.Model):
 
 class Fournisseur(models.Model):
     designation = models.CharField(max_length=100)
-    address = models.CharField(max_length=250, null=True)
+    address = models.CharField(max_length=1000, null=True)
 
     def prices(self):
         return self.price_set.all()
